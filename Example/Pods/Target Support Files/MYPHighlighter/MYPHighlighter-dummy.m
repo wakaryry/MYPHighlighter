a1d7087d702b4fa0f8ff418fabd4ab5bed7a734c
wakary <redoume@163.com>
2018-05-23 21:57:18 +0800

    init

---
+++ Example/Pods/Target Support Files/MYPHighlighter/MYPHighlighter-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MYPHighlighter : NSObject
+@end
+@implementation PodsDummy_MYPHighlighter
+@end
---
+++ Example/Pods/Target Support Files/Pods-MYPHighlighter_Example/Pods-MYPHighlighter_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MYPHighlighter_Example : NSObject
+@end
+@implementation PodsDummy_Pods_MYPHighlighter_Example
+@end
